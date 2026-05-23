48f29a49a1ea18d28f493ea8589c1017e1f33d21
AimanDebug <105602441+AimanDebug@users.noreply.github.com>
2026-05-23 14:17:28 +0200

    chore: format

--- include/config.h
+++ include/config.h
@@ -1,7 +1,7 @@
 /**
  * @brief Configuration for the MapReduce framework.
  * @author Adnaan Juma
-*/
+ */
 #include <assert.h>
 #include <stddef.h>
 
@@ -11,22 +11,26 @@
 #ifndef MR_DEFAULT_MAPPER_THREADS
 #define MR_DEFAULT_MAPPER_THREADS 4
 #endif
-static_assert((size_t)MR_DEFAULT_MAPPER_THREADS > (size_t)0, "Default mapper threads must be at least 1");
+static_assert((size_t)MR_DEFAULT_MAPPER_THREADS > (size_t)0,
+              "Default mapper threads must be at least 1");
 
 #ifndef MR_DEFAULT_REDUCER_THREADS
 #define MR_DEFAULT_REDUCER_THREADS 4 //< Default number of reducer threads
 #endif
-static_assert((size_t)MR_DEFAULT_REDUCER_THREADS > (size_t)0, "Default reducer threads must be at least 1");
+static_assert((size_t)MR_DEFAULT_REDUCER_THREADS > (size_t)0,
+              "Default reducer threads must be at least 1");
 
 #ifndef MR_DEFAULT_QUEUE_SIZE
 #define MR_DEFAULT_QUEUE_SIZE 100 //< Default size of the task queue in elements
 #endif
-static_assert((size_t)MR_DEFAULT_QUEUE_SIZE > (size_t)0, "Default queue size must be at least 1");
+static_assert((size_t)MR_DEFAULT_QUEUE_SIZE > (size_t)0,
+              "Default queue size must be at least 1");
 
 #ifndef MR_DEFAULT_LOG_FILE
 #define MR_DEFAULT_LOG_FILE "libmr.log" //< Default log file name
 #endif
 
-static_assert(sizeof(MR_DEFAULT_LOG_FILE) > (size_t)1, "Default log filename must be non-empty");
+static_assert(sizeof(MR_DEFAULT_LOG_FILE) > (size_t)1,
+              "Default log filename must be non-empty");
 
 #endif // CONFIG_H
